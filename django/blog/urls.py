13a0ed015e223c571ba969d601bd00d2d15acf68
kota7175 <pengon13064@gmail.com>
2019-10-28 15:37:46 +0900

    detail作成

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:blog_id>/publishing/', views.publishing, name='publishing'),
     path('<int:blog_id>/unpublishing/', views.unpublishing, name='unpublishing'),
+    path('<int:blog_id>/detail/', views.detail, name='detail'),
+    path('<int:blog_id>/edit/', views.edit, name='edit'),
 ]
--- django/blog/views.py
+++ django/blog/views.py
@@ -4,8 +4,16 @@ from django.utils import timezone
 
 
 def post_list(request):
-    blogs = Post.objects.order_by('created_date')
-    return render(request, 'blog/post_list.html', {'blogs': blogs})
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def detail(request, blog_id):
+    post = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def edit(request, blog_id):
+    post = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'blog/post_edit.html', {'post': post})
 
 def publishing(request, blog_id):
     blog = get_object_or_404(Post, pk=blog_id)
